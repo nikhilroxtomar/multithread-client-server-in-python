d241e101adc0f1df5c68a90fc9d19be85d44550b
Nikhil Tomar <nikhilroxtomar@gmail.com>
2020-12-27 20:34:59 +0530

    Add files via upload

---
+++ client.py
@@ -0,0 +1,28 @@
+import socket
+
+IP = socket.gethostbyname(socket.gethostname())
+PORT = 5566
+ADDR = (IP, PORT)
+SIZE = 1024
+FORMAT = "utf-8"
+DISCONNECT_MSG = "!DISCONNECT"
+
+def main():
+    client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    client.connect(ADDR)
+    print(f"[CONNECTED] Client connected to server at {IP}:{PORT}")
+
+    connected = True
+    while connected:
+        msg = input("> ")
+
+        client.send(msg.encode(FORMAT))
+
+        if msg == DISCONNECT_MSG:
+            connected = False
+        else:
+            msg = client.recv(SIZE).decode(FORMAT)
+            print(f"[SERVER] {msg}")
+
+if __name__ == "__main__":
+    main()
---
+++ server.py
@@ -0,0 +1,42 @@
+import socket
+import threading
+import wikipedia
+
+IP = socket.gethostbyname(socket.gethostname())
+PORT = 5566
+ADDR = (IP, PORT)
+SIZE = 1024
+FORMAT = "utf-8"
+DISCONNECT_MSG = "!DISCONNECT"
+
+def handle_client(conn, addr):
+    print(f"[NEW CONNECTION] {addr} connected.")
+
+    connected = True
+    while connected:
+        msg = conn.recv(SIZE).decode(FORMAT)
+        if msg == DISCONNECT_MSG:
+            connected = False
+
+        print(f"[{addr}] {msg}")
+        # msg = f"Msg received: {msg}"
+        msg = wikipedia.summary(msg, sentences=1)
+        conn.send(msg.encode(FORMAT))
+
+    conn.close()
+
+def main():
+    print("[STARTING] Server is starting...")
+    server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server.bind(ADDR)
+    server.listen()
+    print(f"[LISTENING] Server is listening on {IP}:{PORT}")
+
+    while True:
+        conn, addr = server.accept()
+        thread = threading.Thread(target=handle_client, args=(conn, addr))
+        thread.start()
+        print(f"[ACTIVE CONNECTIONS] {threading.activeCount() - 1}")
+
+if __name__ == "__main__":
+    main()
